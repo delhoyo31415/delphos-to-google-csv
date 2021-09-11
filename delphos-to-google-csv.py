3760d34b024b309b97c6423225785ecfe90bcaf0
Pablo del Hoyo <pablodelhoyo1314@gmail.com>
2021-09-11 11:39:26 +0200

    Implement -p option

--- delphos-to-google-csv.py
+++ delphos-to-google-csv.py
@@ -20,7 +20,7 @@ import csv
 import secrets
 import argparse
 
-from typing import List, Tuple
+from typing import Dict, List, Tuple, Set
 
 VOWELS_WITH_ACCENT = "áéíóúÁÉÍÓÚ"
 VOWELS_WITHOUT_ACCENT = "aeiouAEIOU"
@@ -35,7 +35,7 @@ ORG_UNIT_PATH = "Org Unit Path [Required]"
 PASSWORD = "Password [Required]"
 CHANGE_PASSWORD = "Change Password at Next Sign-In"
 
-def get_args():
+def get_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         prog="delphos_google_converter",
         description="Convierte csv generado por Delphos al csv que admite Google Suite"
@@ -45,22 +45,24 @@ def get_args():
     parser.add_argument("domain", metavar="dominio", help="Dominio del instituto (ej iesinsti.com)")
     parser.add_argument("year", metavar="año", help="Año del curso académico (ej 2021-2022)")
 
-    parser.add_argument("--profesores", "-p", action="store", 
-                        help=("Nombre archivo de profesores de delphos (csv)"))
+    parser.add_argument("--profesores", "-p", action="store",
+                        help="Nombre archivo de profesores de delphos (csv)")
+    parser.add_argument("--output", "-o", action="store", default="profes.csv",
+                        help="Nombre del csv con los nuevos profesores")
 
     args = parser.parse_args()
 
     return args
 
-def random_number_only_password(digits):
+def random_number_only_password(digits) -> str:
     return "".join([str(secrets.randbelow(10)) for _ in range(digits)])
 
-def remove_accent(letter):
+def remove_accent(letter) -> str:
     if letter in VOWELS_WITH_ACCENT:
         return VOWELS_WITHOUT_ACCENT[VOWELS_WITH_ACCENT.index(letter)]
     return letter
 
-def remove_all_accents(word):
+def remove_all_accents(word) -> str:
     return "".join(remove_accent(letter) for letter in word)
 
 class SchoolPerson:
@@ -70,10 +72,10 @@ class SchoolPerson:
         self.lastname = lastname
         self.password = random_number_only_password(8)
         
-        self.email: str = "{}@" + DOMAIN
+        self.email: str = "{}" + DOMAIN
         self.org_path_unit = PATH
 
-    def as_csv_dict(self):
+    def as_csv_dict(self) -> Dict[str, str]:
         return {
             FIRSTNAME: self.firstname,
             LASTNAME: self.lastname,
@@ -83,12 +85,19 @@ class SchoolPerson:
             CHANGE_PASSWORD: "TRUE"
         }
 
-    def build_email_user(self):
+    @property
+    def fullname(self) -> str:
+        return f"{self.firstname} {self.lastname.strip()}"
+
+    def build_email_user(self) -> str:
         raise NotImplementedError("Método implementado en clases descendientes")
 
     @classmethod
-    def from_csv(cls, csv_data: str):
+    def from_csv(cls, csv_data: str) -> str:
         raise NotImplementedError("Método implementado en clases descendientes")
+
+    def __str__(self) -> str:
+        return f"{self.firstname} {self.lastname} ({self.email})"
 
     def __repr__(self) -> str:
         return (f"{self.__class__.__name__}(firstname={self.firstname}, "
@@ -100,14 +109,25 @@ class Teacher(SchoolPerson):
         super().__init__(firstname, lastname)
         self.email = self.email.format(self.build_email_user())
 
-    def build_email_user(self):
+    def build_email_user(self) -> str:
         first_surname = self.lastname.split()[0]
         return self.firstname[0].lower() + remove_all_accents(first_surname).lower()
 
     @classmethod
     def from_csv(cls, csv_data: str):
         lastname, firstname = csv_data.split(", ")
-        return cls(firstname, lastname)
+        return cls(firstname.strip(), lastname.strip())
+
+def create_teacher_csv(teachers: List[Teacher], csv_filename: str,
+                        fieldnames: str, all_names: Set[str]) -> None:
+    with open(csv_filename, "w") as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames)
+        csv_writer.writeheader()
+        for teacher in teachers:
+            if teacher.fullname not in all_names:
+                print(f"Creando {teacher} en {teacher.org_path_unit}")
+                csv_writer.writerow(teacher.as_csv_dict())
+
 
 def load_teachers(csv_filename: str) -> List[Teacher]:
     with open(csv_filename, encoding="latin_1") as csv_file:
@@ -116,15 +136,18 @@ def load_teachers(csv_filename: str) -> List[Teacher]:
         next(csv_reader)
         return [Teacher.from_csv(data[0]) for data in csv_reader]
 
-def retrieve_google_csv_data(filename: str) -> Tuple[str, set]:
+def retrieve_google_csv_data(filename: str) -> Tuple[str, Set[str]]:
     with open(filename) as csv_filename:
         csv_reader = csv.reader(csv_filename)
-        all_emails = set()
+        all_names = set()
+
         fieldnames = next(csv_reader)
 
         for row in csv_reader:
-            all_emails.add(row[2])
-    return (fieldnames, all_emails)
+            name = f"{row[0].strip()} {row[1].strip()}"
+            all_names.add(name)
+
+    return (fieldnames, all_names)
 
 
 def main():
@@ -136,11 +159,11 @@ def main():
     DOMAIN = DOMAIN.format(args.domain)
     PATH = PATH.format(args.year)
 
-    fieldname, all_emails = retrieve_google_csv_data(args.csv_google)
+    fieldnames, all_names = retrieve_google_csv_data(args.csv_google)
 
     if args.profesores:
-        print(load_teachers(args.profesores))
-
+        all_teachers = load_teachers(args.profesores)
+        create_teacher_csv(all_teachers, args.output, fieldnames, all_names)
     
 
 if __name__ == "__main__":
